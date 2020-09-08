bddd50820f946e27bd9417415b0c61249243fba4
Millo Evers <exedysdigital@gmail.com>
2020-09-08 22:05:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 #import user
 import re
+import random
 
+points = 0
 with open("data/songs.txt", "r") as songs:
-    for line in songs:
-        songName, artistName = line.split(',')
-        #print(f"Song is {songName} by {artistName}")
-        print(re.sub('[^A-Z]', ' _ ', songName))
+    lineCount = songs.read().splitlines()
+    #songName, artistName = line.split(',')
+    #print(f"Song is {songName} by {artistName}")
+
+def addPoints(num: int):
+    points = points + num
+
+def game():
+    line = random.choice(lineCount)
+    songName, artistName = line.split(',')
+    print(re.sub('[^A-Z]', ' _ ', songName))
+    guess = input("What is your guess? ")
+    if guess == songName:
+        addPoints(3)
+        print("Correct!")
+        game()
+    print(f"Your score: {points}")
+
+game()
