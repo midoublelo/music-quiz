20297c371c66fc4330a1648213f97bd737d013aa
Millo Evers <exedysdigital@gmail.com>
2020-09-09 13:21:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
-#import user
 import re
 import random
 
-points = 0
 with open("data/songs.txt", "r") as songs:
     lineCount = songs.read().splitlines()
-    #songName, artistName = line.split(',')
-    #print(f"Song is {songName} by {artistName}")
 
-def addPoints(num: int):
-    points = points + num
+username = ""
+
+def logIn():
+    global username
+    nameInput = input("Enter your username: ")
+    passInput = input("Enter your password: ")
+    with open("data/users.txt", "r") as users:
+        if (nameInput + ":" + passInput + "\n") in users.readlines():
+            print("Logged in successfully.")
+            username = nameInput
+            game()
+        else:
+            print("Username or Password is incorrect!")
+
+points = 0
+
+def gameOver():
+  global username
+  print("Game over!")
+  print(f"Your score: {points}")
+  with open("data/scores.txt", "r"):
+    print("test")
 
 def game():
+    global points
     line = random.choice(lineCount)
     songName, artistName = line.split(',')
-    print(re.sub('[^A-Z]', ' _ ', songName))
+    print(re.sub('[^A-Z]', ' _ ', songName) + f"by {artistName}")
     guess = input("What is your guess? ")
     if guess == songName:
-        addPoints(3)
-        print("Correct!")
+        print("Correct! +3 Points")
+        points = points + 1
         game()
-    print(f"Your score: {points}")
+    else:
+      print(re.sub('[^A-Z]', ' _ ', songName) + f"by {artistName}")
+      guess = input("What is your guess? ")
+      if guess == songName:
+          print("Correct! +1 Points")
+          game()
+      else:
+        gameOver()   
 
-game()
+logIn()
